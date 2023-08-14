ba08a9eb1c17f687d7dffed1fcba32fe45e20985
Victor Torres <vakordvic@gmail.com>
2023-08-14 03:26:47 -0600

    Added some of the line parser functions

--- line_parsers.py
+++ line_parsers.py
@@ -14,7 +14,7 @@ def generic_predicate(bill, line_index, parsed, field_name):
     setattr(bill, field_name, value)
 
 
-def split_predicate(line_index, parsed, split_len):
+def split_predicate(line_index, parsed, bill, split_len):
     tokens = parsed.predicate.split()
     if len(tokens) < split_len:
         error_msg = f"Could not split line in {split_len}"
@@ -24,67 +24,84 @@ def split_predicate(line_index, parsed, split_len):
             line_index=line_index,
             error=error_msg
         )
-        return error
+        bill.errors.append(error)
+        return None
     return tokens
 
 
 # SECTION 010000
 
 def parse_0100000(bill, line_index, parsed):
-    undefined_line(line_index, parsed)
+    # undefined_line(line_index, parsed)
+    bill.id = line_index
+    bill.segmentacion = parsed.predicate
 
 def parse_0100100(bill, line_index, parsed):
-    undefined_line(line_index, parsed)
+    # undefined_line(line_index, parsed)
+    line = parsed.predicate[82:]
+    if len(line) == 15:
+        line += " " *(29 - 15)
+    
+    bill.person_id = line[-9:].strip()
+    bill.fecha_emision = line[:11]
+    bill.imprime_ciclo = line[11:12]
+    bill.con_inanciamiento = line[12:13]
+    bill.tipoFactura = line[13:14]
+    bill.imprimeBPS = line[14:15]
+    bill.refactura = 'N'
+
+    
 
 parse_0100200 = partial(generic_predicate, field_name="emisor_nombre")
 parse_0100300 = partial(generic_predicate, field_name="emisor_dir_l1")
 parse_0100400 = partial(generic_predicate, field_name="emisor_dir_l2")
 
 def parse_0100500(bill, line_index, parsed):
-    result = split_predicate(line_index, parsed, 3)
+    tokens = split_predicate(line_index, parsed, bill, 3)
     
-    if type(result) != list:
-        bill.errors.append(result)
+    if not tokens:
+        return
 
     bill.emisor_dir_l3 = tokens[0]
     bill.emisor_NIT = tokens[2]
 
 def parse_0100600(bill, line_index, parsed):
-    result = split_predicate(line_index, parsed, 3)
-    
-    if type(result) != list:
-        bill.errors.append(result)
+    tokens = split_predicate(line_index, parsed, bill, 3)
 
-    bill.factura_serie = result[2]
+    if not tokens:
+        return
+
+    bill.factura_serie = tokens[2]
     
-    if sectionRecord.facturaSerie == 'NUMERO':
-        sectionRecord.facturaSerie = ' '
+    if bill.factura_serie == 'NUMERO':
+        bill.factura_serie = ' '
     
-    sectionRecord.numeroAutorizacion = tokens[-1]
-    if sectionRecord.numeroAutorizacion == 'AUTORIZACION:':
-        sectionRecord.numeroAutorizacion = ' '
+    bill.numero_autorizacion = tokens[-1]
+    if bill.numero_autorizacion == 'AUTORIZACION:':
+        bill.numero_autorizacion = ' '
     
 
 def parse_0100700(bill, line_index, parsed):
-    result = split_predicate(line_index, parsed, 4)
+    tokens = split_predicate(line_index, parsed, bill, 4)
     
-    if type(result) != list:
-        bill.errors.append(result)
+    if not tokens:
+        return
     
-    sectionRecord.documentoNumero = tokens[1]
+    bill.documento_numero = tokens[1]
 
-    if sectionRecord.documentoNumero == 'PARA':
-        sectionRecord.documentoNumero = ' '
-        sectionRecord.documentoNumeroV2 = sectionRecord.documentoNumero.replace(',', '')
-        sectionRecord.consultas = " ".join(tokens[1:-1])
+    if bill.documento_numero == 'PARA':
+        bill.documento_numero = ' '
+        bill.documento_numero_V2 = bill.documento_numero.replace(',', '')
+        bill.consultas = " ".join(tokens[1:-1])
     
     else:
-        sectionRecord.documentoNumeroV2 = sectionRecord.documentoNumero.replace(',', '')
-        sectionRecord.consultas = " ".join(tokens[2:-1])
+        bill.documento_numero_V2 = bill.documento_numero.replace(',', '')
+        bill.consultas = " ".join(tokens[2:-1])
 
 
 def parse_0100800(bill, line_index, parsed):
     undefined_line(line_index, parsed)
+    # tokens = split_predicate(line_index, parsed,)
 
 def parse_0100900(bill, line_index, parsed):
     undefined_line(line_index, parsed)
